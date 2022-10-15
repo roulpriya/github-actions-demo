c9463d038972e8da06606f6870de5b960df5fbc9
Priyambada <priyambada.roul@batonsystems.com>
2022-10-15 20:02:13 +0530

    Initial commit

---
+++ src/main/java/com/example/githubdemoapp/GithubDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDemoAppApplication.class, args);
+	}
+
+}
